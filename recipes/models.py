cd2e57f3c8979d0bd8645266271ecd6a8ddaae53
alexroque <alexroquesc@yahoo.com.br>
2022-10-01 11:47:27 -0300

    commit até aula 53

---
+++ recipes/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length = 65)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length = 65)
+    description = models.CharField(max_length = 165)
+    slug = models.SlugField()
+    prepation_time = models.IntegerField()
+    prepation_time_unit = models.CharField(max_length = 65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length = 65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+    is_published = models.BooleanField(default = False)
+    cover = models.ImageField(upload_to = 'recipes/covers/%Y/%m/%d')
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null = True)
+    author = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
